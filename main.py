113c127bf969bb5daa644113fb9a8b23f8a95e1a
Paul Bin <24hbin@aes.ac.in>
2022-05-17 08:47:04 +0530

    small changes to turn degrees in task8

--- main.py
+++ main.py
@@ -247,9 +247,9 @@ def task8():  # task 7 + task 4
     bob.lineFollowing(2)
     bob.turnForward(90)
     # have to do a zigzag cause it isn't two black lines
-    bob.lineFollowing(100, terminalTime=timeTillCenter)
+    bob.lineFollowing(100, speed=bob.SLOWMODE, terminalTime=timeTillCenter)
 
-    bob.turnForward(-90)
+    bob.turnForward(-100)
 
     bob.lineFollowing(1)  # in front of the waterbottles
 
